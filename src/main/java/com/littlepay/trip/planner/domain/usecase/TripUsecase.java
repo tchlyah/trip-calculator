58dd9a4b088c70eaa6bb0eb07a1fe861f45bc789
Tayeb Chlyah <tayebchlyah@gmail.com>
2022-04-23 17:30:53 +0200

    ✨ Calculate trips

--- src/main/java/com/littlepay/trip/planner/domain/usecase/TripUsecase.java
+++ src/main/java/com/littlepay/trip/planner/domain/usecase/TripUsecase.java
@@ -2,12 +2,17 @@ package com.littlepay.trip.planner.domain.usecase;
 
 import com.littlepay.trip.planner.domain.model.*;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.littlepay.trip.planner.domain.model.Stop.*;
+import static com.littlepay.trip.planner.domain.model.TapType.OFF;
+import static com.littlepay.trip.planner.domain.model.TapType.ON;
+import static com.littlepay.trip.planner.domain.model.TripStatus.*;
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static java.util.function.Function.identity;
 
 public class TripUsecase {
 
@@ -18,11 +23,54 @@ public class TripUsecase {
 
     private static final Map<Stop, Double> higherPayGrid = initializeHigherPayGrid();
 
-    public List<Trip> calculateTrips(List<Tap> taps) {
-        return null;
+    public static List<Trip> calculateTrips(List<Tap> taps) {
+        return taps.stream()
+                .sorted(Comparator.comparing(Tap::dateTimeUTC))
+                .map(tap ->
+                        new Trip(tap.tapType() == ON ? tap.dateTimeUTC() : null, tap.tapType() == OFF ? tap.dateTimeUTC() : null,
+                                0,
+                                tap.tapType() == ON ? tap.stopId() : null, tap.tapType() == OFF ? tap.stopId() : null,
+                                calculateHigherAmount(tap.stopId()),
+                                tap.companyId(), tap.busId(), tap.pan(), INCOMPLETE))
+                .collect(Collectors.toMap(trip -> trip.companyId() + trip.busId() + trip.pan(), identity(), TripUsecase::mergeTrips))
+                .values().stream()
+                .sorted(Comparator.comparing(Trip::started))
+                .toList();
     }
 
-    static double calculateHigherAmount(Stop from, Stop to) {
+    private static Trip mergeTrips(Trip trip1, Trip trip2) {
+        var started = findFirst(trip1, trip2, Trip::started);
+        var finished = findFirst(trip1, trip2, Trip::finished);
+        var durationSecs = started != null && finished != null ? SECONDS.between(started, finished) : 0L;
+        var from = findFirst(trip1, trip2, Trip::fromStopId);
+        var to = findFirst(trip1, trip2, Trip::toStopId);
+        TripStatus status;
+        double chargeAmount;
+
+        if (from == null) {
+            throw new IllegalArgumentException("Trip should start somewhere");
+        }
+
+        if (from == to) {
+            status = CANCELLED;
+            chargeAmount = 0d;
+        } else {
+            status = COMPLETED;
+            chargeAmount = calculateAmount(from, to);
+        }
+
+        return new Trip(started, finished, durationSecs, from, to, chargeAmount, trip1.companyId(), trip1.busId(), trip1.pan(), status);
+    }
+
+    private static <T> T findFirst(Trip trip1, Trip trip2, Function<Trip, T> getter) {
+        return Stream.of(trip1, trip2)
+                .map(getter)
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElse(null);
+    }
+
+    static double calculateAmount(Stop from, Stop to) {
         return payGrid.get(new Tuple<>(from, to));
     }
 
--- src/test/java/com/littlepay/trip/planner/domain/usecase/TripUsecaseTest.java
+++ src/test/java/com/littlepay/trip/planner/domain/usecase/TripUsecaseTest.java
@@ -12,32 +12,65 @@ import java.util.stream.Stream;
 import static com.littlepay.trip.planner.domain.model.Stop.*;
 import static com.littlepay.trip.planner.domain.model.TapType.OFF;
 import static com.littlepay.trip.planner.domain.model.TapType.ON;
-import static com.littlepay.trip.planner.domain.model.TripStatus.COMPLETED;
+import static com.littlepay.trip.planner.domain.model.TripStatus.*;
+import static java.time.temporal.ChronoUnit.MINUTES;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class TripUsecaseTest {
 
-    public static final String COMPANY_1 = "Company1";
-    public static final String BUS_37 = "Bus37";
-    public static final String PAN = "5500005555555559";
+    static final String COMPANY_1 = "Company1";
+    static final String COMPANY_2 = "Company2";
+    static final String BUS_37 = "Bus37";
+    static final String BUS_47 = "Bus47";
+    static final String BUS_19 = "Bus19";
+    static final String PAN_1 = "5500005555555559";
+    static final String PAN_2 = "122000000000003";
+    public static final LocalDateTime DATE_1 = LocalDateTime.of(2022, 1, 22, 13, 0);
 
     static Stream<Arguments> calculateTrips() {
         return Stream.of(
                 arguments(
+                        "COMPLETED",
                         List.of(
-                                new Tap(1, LocalDateTime.of(2022, 1, 22, 13, 0), ON, STOP_1, COMPANY_1, BUS_37, PAN),
-                                new Tap(2, LocalDateTime.of(2022, 1, 22, 13, 5), OFF, STOP_2, COMPANY_1, BUS_37, PAN)),
+                                new Tap(1, DATE_1, ON, STOP_1, COMPANY_1, BUS_37, PAN_1),
+                                new Tap(2, DATE_1.plus(5, MINUTES), OFF, STOP_2, COMPANY_1, BUS_37, PAN_1)),
                         List.of(
-                                new Trip(LocalDateTime.of(2022, 1, 22, 13, 0), LocalDateTime.of(2022, 1, 22, 13, 5), 900, STOP_1, STOP_2, 3.25, COMPANY_1, BUS_37, PAN, COMPLETED)
+                                new Trip(DATE_1, DATE_1.plus(5, MINUTES), 300, STOP_1, STOP_2, 3.25, COMPANY_1, BUS_37, PAN_1, COMPLETED)
+                        )),
+                arguments(
+                        "INCOMPLETE",
+                        List.of(
+                                new Tap(1, DATE_1, ON, STOP_1, COMPANY_1, BUS_37, PAN_1)),
+                        List.of(
+                                new Trip(DATE_1, null, 0, STOP_1, null, 7.3, COMPANY_1, BUS_37, PAN_1, INCOMPLETE)
+                        )),
+                arguments(
+                        "CANCELED",
+                        List.of(
+                                new Tap(1, DATE_1, ON, STOP_1, COMPANY_1, BUS_37, PAN_1),
+                                new Tap(2, DATE_1.plus(5, MINUTES), OFF, STOP_1, COMPANY_1, BUS_37, PAN_1)),
+                        List.of(
+                                new Trip(DATE_1, DATE_1.plus(5, MINUTES), 300, STOP_1, STOP_1, 0, COMPANY_1, BUS_37, PAN_1, CANCELLED)
+                        )),
+                arguments(
+                        "CANCELED, Different pans",
+                        List.of(
+                                new Tap(1, DATE_1, ON, STOP_1, COMPANY_1, BUS_37, PAN_1),
+                                new Tap(2, DATE_1.plus(5, MINUTES), ON, STOP_2, COMPANY_1, BUS_37, PAN_2)),
+                        List.of(
+                                new Trip(DATE_1, null, 0, STOP_1, null, 7.3, COMPANY_1, BUS_37, PAN_1, INCOMPLETE),
+                                new Trip(DATE_1.plus(5, MINUTES), null, 0, STOP_2, null, 5.5, COMPANY_1, BUS_37, PAN_2, INCOMPLETE)
                         ))
         );
     }
 
-    //    @MethodSource
-//    @ParameterizedTest
-    void calculateTrips(List<Tap> taps, List<Trip> expectedTrips) {
-
+    @MethodSource
+    @ParameterizedTest(name = "{0}")
+    void calculateTrips(String description, List<Tap> taps, List<Trip> expectedTrips) {
+        assertThat(TripUsecase.calculateTrips(taps))
+                .as(description)
+                .containsExactlyElementsOf(expectedTrips);
     }
 
     static Stream<Arguments> calculateAmount() {
@@ -54,7 +87,7 @@ class TripUsecaseTest {
     @MethodSource
     @ParameterizedTest
     void calculateAmount(Stop from, Stop to, Double expected) {
-        assertThat(TripUsecase.calculateHigherAmount(from, to)).isEqualTo(expected);
+        assertThat(TripUsecase.calculateAmount(from, to)).isEqualTo(expected);
     }
 
     static Stream<Arguments> calculateHigherAmount() {
